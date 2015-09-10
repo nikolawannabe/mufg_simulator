35987bf0e8f251c04a586e61494709bd5c7dd01a
Case Talbot <ctalbot@smashwords.com>
2015-09-09 18:01:37 -0700

    Add an interface for the two options
    Fix some bugs

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # mufg_simulator
 Simulator for production of items in a MUFG
+
+
+# Examples
+```
+$ python mufg_simulator.py 
+Calculate items gained in N days (calcitems) or Run some tests about how whether it's better to use multiple capsules or one (testcapsules):     
+how many items do you have at the start?: 24
+how many days do you have to multiple them?: 90
+average items after  90  days over 1000  trials 58.386
+This assumes that you are moving items to new MUFG capsules when the capsule reaches 90 or so items and keeping your inventory from becoming overfull.
+```
+
+```
+ctalbot@frankland ~/src/ingress/mufg_simulator  (master)  ❄ python mufg_simulator.py 
+Calculate items gained in N days (calcitems) or Run some tests about how whether it's better to use multiple capsules or one (testcapsules): testcapsules
+how many days to wait (default 90) 
+how many items altogether? (default 24) 
+how many capsules to split them into (default 2) 
+Running  1000 trials for two capsules split: ...
+Using separate capsules, on average we would get:  58.832
+Running 1000 trials for items together: ...
+Putting items together, on average we would get:  58.829
+```
--- mufg_simulator.py
+++ mufg_simulator.py
@@ -14,7 +14,7 @@ def iterate_n_days(n, initial_items):
 #      print "day=",y," m=",current_items
   return current_items
 
-def how_many_in_n_days():
+def how_many_in_n_days_text_wrapper():
   initial_items = int(raw_input("how many items do you have at the start?: ") or "50")
   iteration_days = int(raw_input("how many days do you have to multiple them?: ") or "100")
   trials = 1000
@@ -31,22 +31,29 @@ def how_many_in_n_days(initial_items, iteration_days, trials):
   return average_items_accumulated
  
 
-def how_many_in_n_capsules(initial_items, number_of_capsules):
-  number_of_capsules = number_of_capsules
-  each_capsule_items = int(initial_items / number_of_capsules)
-  days_to_wait = 180
+def how_many_in_n_capsules():
   trials = 1000
-  
-  print "Running trials for two capsules split: ..."
+   
+  days_to_wait = int(raw_input("how many days to wait (default 90) ") or "90")
+  initial_items = int(raw_input("how many items altogether? (default 24) ") or "24")
+  number_of_capsules = int(raw_input("how many capsules to split them into (default 2) ") or "2")
+  each_capsule_items = int(initial_items / number_of_capsules)
+  print "Running ", trials, "trials for two capsules split: ..."
   total_items_accumulated = 0
   for capsule in range(0,number_of_capsules):
     total_items_accumulated = total_items_accumulated + how_many_in_n_days(each_capsule_items, days_to_wait, trials)
   print "Using separate capsules, on average we would get: ", total_items_accumulated
 
-  print "Running trials for items together: ..."
+  print "Running", trials, "trials for items together: ..."
   total_items_accumulated = how_many_in_n_days(initial_items, days_to_wait, trials)
   print "Putting items together, on average we would get: ", total_items_accumulated
 
-initial_items = 4
-number_of_capsules = 2
-how_many_in_n_capsules(initial_items, number_of_capsules)
+def main():
+  task = raw_input("Calculate items gained in N days (calcitems) or Run some tests about how whether it's better to use multiple capsules or one (testcapsules): ") or "calcitems"  
+  if task == "calcitems":
+    how_many_in_n_days_text_wrapper()
+  elif task == "testcapsules":
+    how_many_in_n_capsules()
+
+if __name__ == "__main__":
+    main()
